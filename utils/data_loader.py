524cd140a08e1dcf27d9e6254d7acdee52c96c9c
AKASH M S <manigarakash@gmail.com>
2026-04-14 13:42:44 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,8 +2,5 @@ import pandas as pd
 
 def load_data(path="data/sample_data.csv"):
     df = pd.read_csv(path, encoding='ISO-8859-1')
-
-    print("DEBUG columns:", df.columns)  # 👈 IMPORTANT
-
-    df['date'] = pd.to_datetime(df['date'])
+    df['date'] = pd.to_datetime(df['date']).dt.date
     return df
